987220ef205d3bd8a7e59ed80dea55f85377feb2
nafiseggs <157635384+nafiseggs@users.noreply.github.com>
2024-03-11 09:23:18 +0600

    index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.post('/apikey', async (req, res) => {
+    try {
+        const { url } = req.body;
+        const response = await axios.post('https://all-video-downloader1.p.rapidapi.com/xnxx', {
+            urlXNXX: url
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-RapidAPI-Key': 'c430b8ecfbmsh1fb735a97be99a5p198e4bjsnd8ce5972748f',
+                'X-RapidAPI-Host': 'all-video-downloader1.p.rapidapi.com'
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
